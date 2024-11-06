1ac5ddab4644c14653d24d67ba2cdc72289a9796
MrCrowNFT <mrcrownft@gmail.com>
2024-11-06 09:03:07 -0300

    form style fix

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
- import (
-	"net/http"
+
+import (
+	"fmt"
+	"go/constant"
 	"html/template"
- )
+	"log"
+	"net/http"
+)
+
+type Url_pair struct {
+	url string;
+	s_url string;
+}
 
  func main(){
-	
+	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		t , err := template.ParseFiles("./UI/FrontPage.html")
+		if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		err = t.Execute()
+		if err != nil{
+			fmt.Println(err)
+		
+		}
+	})
+	log.Fatal(http.ListenAndServe(":5500", nil))
  }
